0556340a49b1a27d4df8b41b840dc7c64a54919d
Дмитрий ГРушевский <mr.jondow@bk.ru>
2024-12-05 15:59:28 +0300

    обновление с добавлением новых столбцов + запись простановки первого слота

--- src/main/java/by/base/main/controller/ajax/MainRestController.java
+++ src/main/java/by/base/main/controller/ajax/MainRestController.java
@@ -308,9 +308,6 @@ public class MainRestController {
 	private static final String loginMarket = "SpeedLogist";
 	private static final String passwordMarket = "12345678";
 	
-	// test test
-
-	//test changed3
 
 
 	public static final Comparator<Address> comparatorAddressId = (Address e1, Address e2) -> (e1.getIdAddress() - e2.getIdAddress());

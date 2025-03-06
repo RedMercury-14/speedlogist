9d54073c1771267685bd981e7ecb41570d8a01dd
Дмитрий ГРушевский <mr.jondow@bk.ru>
2025-03-06 12:28:57 +0300

    добавлены начальные метод для фьжена

--- src/main/java/by/base/main/controller/ajax/FusionController.java
+++ src/main/java/by/base/main/controller/ajax/FusionController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,9 +21,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import by.base.main.aspect.TimedExecution;
 import by.base.main.dto.AuthRequest;
 import by.base.main.dto.AuthResponse;
 import by.base.main.security.JwtTokenProvider;
+import by.base.main.service.ScheduleService;
 
 @RestController
 @RequestMapping(path = "fusion", produces = "application/json")
@@ -31,6 +34,9 @@ public class FusionController {
 	private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserDetailsService userDetailsService;
+    
+    @Autowired
+    ScheduleService scheduleService; 
     
     public FusionController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserDetailsService userDetailsService) {
         this.authenticationManager = authenticationManager;
@@ -59,6 +65,22 @@ public class FusionController {
 
         return new AuthResponse(token);
     }
+	
+	@GetMapping("/schedule/getListTOAll")
+	public Map<String, Object> getListDeliveryScheduleTO(HttpServletRequest request) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("status", "200");
+		response.put("body", scheduleService.getSchedulesListTOAll());
+		return response;		
+	}
+	
+	@GetMapping("/help")
+	public Map<String, Object> getHelp(HttpServletRequest request) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("status", "200");
+		response.put("/schedule/getListTOAll", "Метод возвращает все графики поставок и временные и удалённые и т.д. (status: 0 - удалён; 10 - создан, ожидает подтверждения; 20 - в работе)");
+		return response;		
+	}
 	
 	
 }

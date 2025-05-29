519df7178c44fc360e6830539c21bb81de555a7b
smthyellow <smth.yellow321@gmail.com>
2025-05-29 14:14:15 +0300

    ignore front

--- src/main/webapp/resources/js2/editUser.js
+++
@@ -1,4 +0,0 @@
-
-var numContract = document.querySelector('#numContractFromServer').value;
-document.querySelector('input[name=numContract]').value = numContract.split(' от ')[0];
-document.querySelector('input[name=dateContract]').value = numContract.split(' от ')[1];
